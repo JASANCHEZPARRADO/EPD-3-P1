d86c8dacca662e9633467ad475b1f8849d907bf0
JASANCHEZPARRADO <sanchezparradoja@gmail.com>
2016-10-21 17:28:30 +0200

    Función Float

--- EntregaEPD03/src/test/java/es/upo/cal/EntregaEPD03/Problema01.java
+++ EntregaEPD03/src/test/java/es/upo/cal/EntregaEPD03/Problema01.java
@@ -20,4 +20,24 @@ public class Problema01 {
 		Assert.assertNotEquals(Integer.toHexString(10), "epd3", "No son iguales");
 
 	}
+	
+	/**
+	 * Testing con floats
+	 */
+	@Test
+	public void TestFloat() throws Exception {
+
+		// 1. Testing del método parseInt
+		Assert.assertNotEquals(Float.parseFloat("epd3"), 1.1f, "Son iguales");
+
+		// 2. Testing del método valueOf
+		Assert.assertFalse(Float.valueOf(1.1f) == 2.2f, "Son diferentes");
+
+		// 3. Testing del método toString
+		Assert.assertEquals(Float.toString(1.2f), "1.2", "Son iguales");
+
+		// 4. Testing del método toHexString
+		Assert.assertNotEquals(Float.toHexString(10.2f), "epd3", "No son iguales");
+
+	}
 }
